81a38f97810f020081f0d7d4626c43fea0caf80e
TsukuiAkio <go1klalaainaaoie65nk15a1lolo@yahoo.co.jp>
2021-12-01 06:56:40 +0900

    Finished Gatsby Tutor Part2

---
+++ src/pages/about.js
@@ -0,0 +1,12 @@
+import * as React from 'react'
+import Layout from '../components/layout'
+
+const Aboutpage = () => {
+    return(
+        <Layout pageTilte="About Me">
+            <p>Hi there! I'm the proud creator  of this site, with Gatsby.</p>
+        </Layout>
+    )
+}
+
+export default Aboutpage
